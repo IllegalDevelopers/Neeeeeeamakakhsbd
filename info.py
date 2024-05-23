4a8a2c56a196b5fa9c491092b7bc015205e9862d
Afzal-pathan <169183007+Afzal-pathan@users.noreply.github.com>
2024-05-23 23:55:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,7 +70,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://afzalpathan:Pathan@pathantest.duvren6.mongodb.net/?retryWrites=true&w=majority&appName=Pathantest")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Order:order@cluster0.aitjsft.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "test")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
