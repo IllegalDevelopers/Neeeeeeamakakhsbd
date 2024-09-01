a78cd6cf76b55afcb23d1bc7ae7450702fd632f0
Afzal-pathan <169183007+Afzal-pathan@users.noreply.github.com>
2024-09-01 13:37:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -142,8 +142,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://muzan-ap-1a3f2b595b85.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://muzan-ap-1a3f2b595b85.herokuapp.com/".format(FQDN, PORT)
+URL = "https://feminist-marketa-muzanbot-ee3c813c.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://feminist-marketa-muzanbot-ee3c813c.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
